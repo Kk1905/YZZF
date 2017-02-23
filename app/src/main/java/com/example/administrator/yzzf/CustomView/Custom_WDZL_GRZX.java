9f84275b7d08e27d8d4ccef643811a7448e2d7a0
likeke <853496784@qq.com>
2017-02-23 21:59:53 +0800

    继续下一个自定义view

---
+++ app/src/main/java/com/example/administrator/yzzf/CustomView/Custom_WDZL_GRZX.java
@@ -0,0 +1,17 @@
+package com.example.administrator.yzzf.CustomView;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.widget.LinearLayout;
+
+import static android.icu.lang.UCharacter.GraphemeClusterBreak.L;
+
+/**
+ * Created by Administrator on 2017/2/23 0023.
+ */
+
+public class Custom_WDZL_GRZX extends LinearLayout {
+    public Custom_WDZL_GRZX(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+}
